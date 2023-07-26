212687c03c9685fc2c97b94d6043b2485e1de557
Hung Nguyen <101444336+hungnguyennaz@users.noreply.github.com>
2023-07-26 21:28:10 +0700

    Accidentally get only host IP

--- src/main/java/me/hungaz/PlayerIPWhitelist.java
+++ src/main/java/me/hungaz/PlayerIPWhitelist.java
@@ -1,19 +1,12 @@
 package me.hungaz;
 
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.sql.*;
 import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
 
 public class PlayerIPWhitelist extends JavaPlugin implements Listener {
     private String mysqlHostname;
@@ -22,15 +15,14 @@ public class PlayerIPWhitelist extends JavaPlugin implements Listener {
     private String mysqlUsername;
     private String mysqlPassword;
     private String kickMessage; // Kick message
-    private int cacheDuration; // Cache duration (in seconds)
 
-    // Separate caches to store IP addresses and their timestamps (in milliseconds)
-    private Map<String, String> ipCache = new HashMap<>();
-    private Map<String, Long> timestampCache = new HashMap<>();
+    // HashMap to store cached results of whitelisted IP addresses
+    private HashMap<String, Long> whitelistCache = new HashMap<>();
+    private int cacheExpiration; // Cache expiration time in seconds
 
     @Override
     public void onEnable() {
-        // Check if a table is exists or not, if not, call createTableIfNotExists()
+        // Check if a table exists or not, if not, call createTableIfNotExists()
         loadConfig();
         if (testMySQLConnection()) {
             getLogger().info("MySQL connected successfully.");
@@ -43,6 +35,7 @@ public class PlayerIPWhitelist extends JavaPlugin implements Listener {
 
     private void loadConfig() {
         // Load the configuration (in config.yml)
+        saveDefaultConfig();
         reloadConfig();
 
         mysqlHostname = getConfig().getString("mysql.hostname");
@@ -50,8 +43,8 @@ public class PlayerIPWhitelist extends JavaPlugin implements Listener {
         mysqlDatabase = getConfig().getString("mysql.database");
         mysqlUsername = getConfig().getString("mysql.username");
         mysqlPassword = getConfig().getString("mysql.password");
-        kickMessage = getConfig().getString("kick-message", "Mày là thằng nào?!");
-        cacheDuration = getConfig().getInt("cache-duration", 300);
+        kickMessage = getConfig().getString("kick-message", "Mày là thằng nào?!"); // Kick message
+        cacheExpiration = getConfig().getInt("cache-expiration", 300); // Cache expiration
     }
 
     private boolean testMySQLConnection() {
@@ -84,67 +77,25 @@ public class PlayerIPWhitelist extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onAsyncPlayerPreLogin(AsyncPlayerPreLoginEvent event) {
-        // Use ipify API to get the public IP address from cache or the API
-        String publicIPAddress = getPublicIPAddressFromCacheOrAPI();
-        if (publicIPAddress == null) {
-            getLogger().warning("Failed to retrieve public IP from ipify API.");
-            return;
-        }
-
-        boolean isWhitelisted = checkWhitelist(publicIPAddress);
+        // Get the player's IP address
+        String ipAddress = event.getAddress().getHostAddress();
+        boolean isWhitelisted = checkWhitelist(ipAddress);
         if (!isWhitelisted) {
             event.disallow(AsyncPlayerPreLoginEvent.Result.KICK_OTHER, kickMessage);
-            getLogger().warning("IP " + publicIPAddress + " tried to join, but not whitelisted.");
+            getLogger().warning("IP " + ipAddress + " tried to join, but not whitelisted.");
         }
-    }
-
-    private String getPublicIPAddressFromCacheOrAPI() {
-        // Get public IP address and cache
-        String publicIPAddress = ipCache.get("ip_address");
-        long currentTime = System.currentTimeMillis();
-
-        if (publicIPAddress != null) {
-            long timestamp = timestampCache.getOrDefault("timestamp", 0L);
-            if (currentTime - timestamp <= cacheDuration * 1000L) {
-                return publicIPAddress;
-            } else {
-                ipCache.remove("ip_address");
-                timestampCache.remove("timestamp");
-            }
-        }
-
-        publicIPAddress = getPublicIPAddressFromAPI();
-        if (publicIPAddress != null) {
-            ipCache.put("ip_address", publicIPAddress);
-            timestampCache.put("timestamp", currentTime);
-        }
-
-        return publicIPAddress;
-    }
-
-    private String getPublicIPAddressFromAPI() {
-        // Get public IP of the player
-        try {
-            URL url = new URL("https://api.ipify.org?format=json");
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-
-            if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
-                try (Scanner scanner = new Scanner(conn.getInputStream())) {
-                    String response = scanner.useDelimiter("\\A").next();
-                    JsonObject jsonObject = JsonParser.parseString(response).getAsJsonObject();
-                    return jsonObject.get("ip").getAsString();
-                }
-            } else {
-                getLogger().warning("Failed to retrieve public IP. HTTP response code: " + conn.getResponseCode());
-            }
-        } catch (IOException e) {
-            getLogger().warning("Failed to retrieve public IP. Error: " + e.getMessage());
-        }
-        return null;
     }
 
     private boolean checkWhitelist(String ipAddress) {
+        // Check if the result is already cached and not expired
+        if (whitelistCache.containsKey(ipAddress)) {
+            long currentTime = System.currentTimeMillis();
+            long cacheTime = whitelistCache.get(ipAddress);
+            if (cacheTime + (cacheExpiration * 1000) > currentTime) {
+                return true;
+            }
+        }
+
         // Find IP address in database
         try (Connection connection = DriverManager.getConnection(
                 "jdbc:mysql://" + mysqlHostname + ":" + mysqlPort + "/" + mysqlDatabase,
@@ -154,7 +105,10 @@ public class PlayerIPWhitelist extends JavaPlugin implements Listener {
                 statement.setString(1, ipAddress);
                 try (ResultSet resultSet = statement.executeQuery()) {
                     if (resultSet.next()) {
-                        return resultSet.getInt(1) > 0;
+                        boolean isWhitelisted = resultSet.getInt(1) > 0;
+                        // Cache the result with the current timestamp
+                        whitelistCache.put(ipAddress, System.currentTimeMillis());
+                        return isWhitelisted;
                     }
                 }
             }
@@ -163,5 +117,4 @@ public class PlayerIPWhitelist extends JavaPlugin implements Listener {
         }
         return false;
     }
-
 }

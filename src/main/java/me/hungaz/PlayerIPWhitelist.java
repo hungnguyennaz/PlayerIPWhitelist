02b9f66db15435ebcfdbcf5dad9c688c14bfe73f
Hung Nguyen <101444336+hungnguyennaz@users.noreply.github.com>
2023-07-26 10:40:36 +0700

    Add cache

--- src/main/java/me/hungaz/PlayerIPWhitelist.java
+++ src/main/java/me/hungaz/PlayerIPWhitelist.java
@@ -1,5 +1,7 @@
 package me.hungaz;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
@@ -9,10 +11,9 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
-
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 
 public class PlayerIPWhitelist extends JavaPlugin implements Listener {
     private String mysqlHostname;
@@ -20,10 +21,16 @@ public class PlayerIPWhitelist extends JavaPlugin implements Listener {
     private String mysqlDatabase;
     private String mysqlUsername;
     private String mysqlPassword;
-    private String kickMessage;
+    private String kickMessage; // Kick message
+    private int cacheDuration; // Cache duration (in seconds)
+
+    // Separate caches to store IP addresses and their timestamps (in milliseconds)
+    private Map<String, String> ipCache = new HashMap<>();
+    private Map<String, Long> timestampCache = new HashMap<>();
 
     @Override
     public void onEnable() {
+        // Check if a table is exists or not, if not, call createTableIfNotExists()
         loadConfig();
         if (testMySQLConnection()) {
             getLogger().info("MySQL connected successfully.");
@@ -35,16 +42,20 @@ public class PlayerIPWhitelist extends JavaPlugin implements Listener {
     }
 
     private void loadConfig() {
+        // Load the configuration (in config.yml)
+        reloadConfig();
+
         mysqlHostname = getConfig().getString("mysql.hostname");
         mysqlPort = getConfig().getInt("mysql.port");
         mysqlDatabase = getConfig().getString("mysql.database");
         mysqlUsername = getConfig().getString("mysql.username");
         mysqlPassword = getConfig().getString("mysql.password");
         kickMessage = getConfig().getString("kick-message", "Mày là thằng nào?!");
+        cacheDuration = getConfig().getInt("cache-duration", 300);
     }
 
     private boolean testMySQLConnection() {
-        try (Connection connection = DriverManager.getConnection(
+        try (Connection ignored = DriverManager.getConnection(
                 "jdbc:mysql://" + mysqlHostname + ":" + mysqlPort + "/" + mysqlDatabase,
                 mysqlUsername, mysqlPassword)) {
             return true;
@@ -55,6 +66,7 @@ public class PlayerIPWhitelist extends JavaPlugin implements Listener {
     }
 
     private void createTableIfNotExists() {
+        // Create a table
         try (Connection connection = DriverManager.getConnection(
                 "jdbc:mysql://" + mysqlHostname + ":" + mysqlPort + "/" + mysqlDatabase,
                 mysqlUsername, mysqlPassword)) {
@@ -72,7 +84,8 @@ public class PlayerIPWhitelist extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onAsyncPlayerPreLogin(AsyncPlayerPreLoginEvent event) {
-        String publicIPAddress = getPublicIPAddress();
+        // Use ipify API to get the public IP address from cache or the API
+        String publicIPAddress = getPublicIPAddressFromCacheOrAPI();
         if (publicIPAddress == null) {
             getLogger().warning("Failed to retrieve public IP from ipify API.");
             return;
@@ -85,7 +98,32 @@ public class PlayerIPWhitelist extends JavaPlugin implements Listener {
         }
     }
 
-    private String getPublicIPAddress() {
+    private String getPublicIPAddressFromCacheOrAPI() {
+        // Get public IP address and cache
+        String publicIPAddress = ipCache.get("ip_address");
+        long currentTime = System.currentTimeMillis();
+
+        if (publicIPAddress != null) {
+            long timestamp = timestampCache.getOrDefault("timestamp", 0L);
+            if (currentTime - timestamp <= cacheDuration * 1000L) {
+                return publicIPAddress;
+            } else {
+                ipCache.remove("ip_address");
+                timestampCache.remove("timestamp");
+            }
+        }
+
+        publicIPAddress = getPublicIPAddressFromAPI();
+        if (publicIPAddress != null) {
+            ipCache.put("ip_address", publicIPAddress);
+            timestampCache.put("timestamp", currentTime);
+        }
+
+        return publicIPAddress;
+    }
+
+    private String getPublicIPAddressFromAPI() {
+        // Get public IP of the player
         try {
             URL url = new URL("https://api.ipify.org?format=json");
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -107,6 +145,7 @@ public class PlayerIPWhitelist extends JavaPlugin implements Listener {
     }
 
     private boolean checkWhitelist(String ipAddress) {
+        // Find IP address in database
         try (Connection connection = DriverManager.getConnection(
                 "jdbc:mysql://" + mysqlHostname + ":" + mysqlPort + "/" + mysqlDatabase,
                 mysqlUsername, mysqlPassword)) {
@@ -124,4 +163,5 @@ public class PlayerIPWhitelist extends JavaPlugin implements Listener {
         }
         return false;
     }
+
 }

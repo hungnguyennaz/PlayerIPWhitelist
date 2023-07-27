57ad17c29d722040be6a22bcc2e9aa8cb4f1e1f4
Hung Nguyen <101444336+hungnguyennaz@users.noreply.github.com>
2023-07-27 14:57:46 +0700

    Allow to enable/disable ignore local ips

--- src/main/java/me/hungaz/PlayerIPWhitelist.java
+++ src/main/java/me/hungaz/PlayerIPWhitelist.java
@@ -5,6 +5,8 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.sql.*;
 import java.util.HashMap;
 
@@ -19,32 +21,36 @@ public class PlayerIPWhitelist extends JavaPlugin implements Listener {
     // HashMap to store cached results of whitelisted IP addresses
     private final HashMap<String, CacheEntry> whitelistCache = new HashMap<>();
     private int cacheExpiration; // Cache expiration time in seconds
+    private boolean doNotignoreLocalIPEnabled; // New variable to store the value of do-not-ignore-local-ip option
+    private boolean logIgnoredLocalIP; // New variable to store the value of log-ignored-local-ip option
 
     @Override
     public void onEnable() {
-        // Check if a table exists or not, if not, call createTableIfNotExists()
-        loadConfig();
+        // Load the configuration (in config.yml)
+        saveDefaultConfig();
+        loadConfigSettings();
+
         if (testMySQLConnection()) {
             getLogger().info("MySQL connected successfully.");
         } else {
             getLogger().warning("MySQL connection failed!");
         }
+
         createTableIfNotExists();
         getServer().getPluginManager().registerEvents(this, this);
     }
 
-    private void loadConfig() {
-        // Load the configuration (in config.yml)
-        saveDefaultConfig();
-        reloadConfig();
-
+    private void loadConfigSettings() {
+        // Configuration variables
         mysqlHostname = getConfig().getString("mysql.hostname");
         mysqlPort = getConfig().getInt("mysql.port");
         mysqlDatabase = getConfig().getString("mysql.database");
         mysqlUsername = getConfig().getString("mysql.username");
         mysqlPassword = getConfig().getString("mysql.password");
-        kickMessage = getConfig().getString("kick-message", "Mày là thằng nào?!"); // Kick message
+        kickMessage = getConfig().getString("kick-message", "Your IP isn't whitelisted!!!"); // Kick message
         cacheExpiration = getConfig().getInt("cache-expiration", 300); // Cache expiration
+        doNotignoreLocalIPEnabled = getConfig().getBoolean("do-not-ignore-local-ip.enabled", true);
+        logIgnoredLocalIP = getConfig().getBoolean("do-not-ignore-local-ip.log-ignored-local-ip", true);
     }
 
     private static class CacheEntry {
@@ -65,7 +71,7 @@ public class PlayerIPWhitelist extends JavaPlugin implements Listener {
         }
     }
 
-    private boolean testMySQLConnection() {
+    public boolean testMySQLConnection() {
         try (Connection ignored = DriverManager.getConnection(
                 "jdbc:mysql://" + mysqlHostname + ":" + mysqlPort + "/" + mysqlDatabase,
                 mysqlUsername, mysqlPassword)) {
@@ -95,12 +101,23 @@ public class PlayerIPWhitelist extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onAsyncPlayerPreLogin(AsyncPlayerPreLoginEvent event) {
-        // Get the player's IP address
+        // Get the player's IP address and name
         String ipAddress = event.getAddress().getHostAddress();
+        String playerName = event.getName();
+
+        if (doNotignoreLocalIPEnabled && isLocalIpAddress(ipAddress)) {
+            if (logIgnoredLocalIP) {
+                getLogger().info("Player " + playerName + " with IP " + ipAddress + " connected through a local IP, ignoring.");
+            }
+            // Prevent whitelist check for local IP addresses
+            event.disallow(AsyncPlayerPreLoginEvent.Result.KICK_OTHER, kickMessage);
+            return;
+        }
+
         boolean isWhitelisted = checkWhitelist(ipAddress);
         if (!isWhitelisted) {
             event.disallow(AsyncPlayerPreLoginEvent.Result.KICK_OTHER, kickMessage);
-            getLogger().warning("IP " + ipAddress + " tried to join, but not whitelisted.");
+            getLogger().warning("Player " + playerName + " with IP " + ipAddress + " tried to join, but not whitelisted.");
         }
     }
 
@@ -115,7 +132,17 @@ public class PlayerIPWhitelist extends JavaPlugin implements Listener {
             }
         }
 
-        // Find IP address in database
+        // Skip database query for local IP addresses
+        if (isLocalIpAddress(ipAddress)) {
+            if (logIgnoredLocalIP) {
+                getLogger().info("Player with IP " + ipAddress + " connected through a local IP, ignoring.");
+            }
+            // Cache the result for local IP address as whitelisted, with the current timestamp
+            whitelistCache.put(ipAddress, new CacheEntry(true, System.currentTimeMillis()));
+            return true;
+        }
+
+        // Find IP address in the database
         try (Connection connection = DriverManager.getConnection(
                 "jdbc:mysql://" + mysqlHostname + ":" + mysqlPort + "/" + mysqlDatabase,
                 mysqlUsername, mysqlPassword)) {
@@ -135,5 +162,15 @@ public class PlayerIPWhitelist extends JavaPlugin implements Listener {
             e.printStackTrace();
         }
         return false;
+    }
+
+    private boolean isLocalIpAddress(String ipAddress) {
+        try {
+            InetAddress addr = InetAddress.getByName(ipAddress);
+            return addr.isLoopbackAddress() || addr.isSiteLocalAddress() || addr.isAnyLocalAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 }
